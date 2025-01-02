2a5a5a8b7686855df97fb4c2937ee2b6f255223c
VORline <vnikita200205@mail.ru>
2025-01-02 13:08:40 +0800

    Add files via upload

---
+++ date_changer.py
@@ -0,0 +1,9 @@
+user_name = 'Имя пользователя'
+title = 'Заголовок заметки'
+content = 'Описание заметки'
+status = 'Статус заметки'
+created_date = '02.01.2025'
+issue_date = '31.12.2025'
+
+print(created_date[0:5], issue_date[0:5], sep='-')
+print(issue_date[0:5])
