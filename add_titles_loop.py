f55c26131b2d78f4042f192610167596de68999a
VORline <vnikita200205@mail.ru>
2025-01-23 09:18:15 +0800

    Add files via upload

--- add_titles_loop.py
+++ add_titles_loop.py
@@ -1,18 +1,19 @@
-titles = []
-i = 0
+titles = [] #список для ввода заголовоков
+i = 0 #бесконечный цикл
 while True:
-    title = input(f'Lобавьте заголовок №{i + 1}\n'
+    title = input(f'Добавьте заголовок №{i + 1}\n'
                   ' (Enter для отмены): ')
     i += 1
+    # условие для остановки программы
     if title == '':
         break
     titles.append(title)
-    print('Вы ввели: ', title)
+    print('Вы ввели: ', title) #отображение введенного значения
     if len(titles) == len(set(titles)):
         continue
     elif len(titles) != len(set(titles)):
         titles.pop(-1)
         print('Вы ввели повторяющийся заголовок!\n',
               'Он будет удалён: ', title)
-print("Ваши заголовки: ")
+print("Ваши заголовки: ") #вывод всех введенных заголовоков
 print('\n'.join(titles))
